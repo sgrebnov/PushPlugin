56be7b633d408c358e9cda6eeab3a91bbced3494
sgrebnov <sergei.grebnov@gmail.com>
2014-02-27 17:01:07 +0400

    adds support of wp8 and windows8

---
+++ src/windows8/PushPluginProxy.js
@@ -0,0 +1,16 @@
+module.exports = {
+    register: function (success, fail, args) {
+        try {
+            var onNotificationReceived = window[args[0].ecb];
+
+            Windows.Networking.PushNotifications.PushNotificationChannelManager.createPushNotificationChannelForApplicationAsync().then(
+                function (channel) {
+                    channel.addEventListener("pushnotificationreceived", onNotificationReceived);
+                    success(channel);
+            }, fail);
+        } catch(ex) {
+            fail(ex);
+        }
+    }
+};
+require("cordova/windows8/commandProxy").add("PushPlugin", module.exports);
